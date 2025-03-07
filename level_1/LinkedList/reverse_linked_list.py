c0edc2d4270e483b01bbd9feedb1bdef3a0fd7f4
Ehaab Basil <54548391+ehaabbasil@users.noreply.github.com>
2025-03-07 16:23:30 -0500

    Reverse Linked List

---
+++ level_1/LinkedList/reverse_linked_list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Initialize Pointers
+        prev = None # Previous Node starts as None
+        curr = head  # Current node start at the head 
+
+        # Traverse the list 
+        while curr is not None:
+            next_node = curr.next # save the next node
+
+            curr.next = prev  # reverse the link
+
+            # Move pointers forward
+            prev = curr    # Move prev to the current node
+            curr = next_node  # Move curr to the next node
+        # prev is now the new head of the reversed list  
+        return prev
+        
+
